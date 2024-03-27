8646edc34f6d18c33fa6e6e260adace373db5ee4
nathfavour <116535483+nathfavour@users.noreply.github.com>
2024-03-27 13:48:35 +0100

    use single instead of microservices

--- microservices/vexeroo/src/routes/product.model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose_1 = require("mongoose");
-var productSchema = new mongoose_1.default.Schema({
-    name: { type: String, required: true, unique: true },
-    // ... other product fields
-});
-exports.default = mongoose_1.default.model('Product', productSchema);
